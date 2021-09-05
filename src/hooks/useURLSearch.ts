ca209c3b806793c204f01fcaca8edabbb4106f9e
zsrilh <952177238@qq.com>
2021-09-05 13:44:48 +0800

    feat: add eslint

---
+++ src/hooks/useURLSearch.ts
@@ -0,0 +1,14 @@
+import { useMemo } from 'react';
+import { parse, stringify, StringifyOptions, ParseOptions } from 'query-string';
+
+// export type UseURLSearchOptions = Pick<
+//   StringifyOptions,
+//   keyof StringifyOptions & keyof ParseOptions
+// >;
+// const useURLSearch = (options?: UseURLSearchOptions): [
+//   Record<string, any>,
+//   (value: Record<string, any>, type?: 'push' | 'replace') => void
+// ] => {
+
+// }
+
